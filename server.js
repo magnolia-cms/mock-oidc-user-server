75959871ea9c961213fdfea762d24007e7631ca3
Maxime Michel <maxime.michel@magnolia-cms.com>
2021-07-14 15:19:56 +0200

    QA server.js: remove semicolons

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 /* eslint-disable no-console */
 
-const assert = require('assert');
-const camelCase = require('camelcase');
+const assert = require('assert')
+const camelCase = require('camelcase')
 
-const Provider = require('oidc-provider');
+const Provider = require('oidc-provider')
 
-const host = process.env.HOST || 'localhost';
-const port = process.env.PORT || 3000;
+const host = process.env.HOST || 'localhost'
+const port = process.env.PORT || 3000
 
 const config = ['CLIENT_ID', 'CLIENT_SECRET', 'CLIENT_REDIRECT_URI', 'CLIENT_LOGOUT_REDIRECT_URI'].reduce((acc, v) => {
-  assert(process.env[v], `${v} config missing`);
-  acc[camelCase(v)] = process.env[v];
-  return acc;
-}, {});
+  assert(process.env[v], `${v} config missing`)
+  acc[camelCase(v)] = process.env[v]
+  return acc
+}, {})
 
 const oidcConfig = {
   features: {
@@ -27,9 +27,9 @@ const oidcConfig = {
     AccessToken: 'jwt',
     RefreshToken: 'jwt'
   }
-};
+}
 
-const oidc = new Provider(`http://${host}:${port}`, oidcConfig);
+const oidc = new Provider(`http://${host}:${port}`, oidcConfig)
 
 const clients = [
   {
@@ -38,19 +38,19 @@ const clients = [
     redirect_uris: [config.clientRedirectUri],
     post_logout_redirect_uris: [config.clientLogoutRedirectUri]
   }
-];
+]
 
 let server;
 (async () => {
-  await oidc.initialize({ clients });
+  await oidc.initialize({ clients })
 
   server = oidc.listen(port, () => {
     console.log(
       `mock-oidc-user-server listening on port ${port}, check http://${host}:${port}/.well-known/openid-configuration`
-    );
-  });
+    )
+  })
 })().catch(err => {
-  if (server && server.listening) server.close();
-  console.error(err);
-  process.exitCode = 1;
-});
+  if (server && server.listening) server.close()
+  console.error(err)
+  process.exitCode = 1
+})
