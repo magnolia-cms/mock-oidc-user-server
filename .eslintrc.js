089383a521cc3a9c90bd5fd57a4394e4d37357b2
Maxime Michel <maxime.michel@magnolia-cms.com>
2021-07-14 14:52:15 +0200

    Remove unused files

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: 'airbnb-base',
-  rules: {
-    'comma-dangle': 0,
-    'arrow-parens': 0
-  }
-};
--- SECURITY.md
+++
@@ -1,5 +0,0 @@
-# Security Policy
-
-## Reporting a Vulnerability
-
-Please contact security@appvia.io
