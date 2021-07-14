3a63bd2d5762f5aa4fee3f8969ef8f4dcd3812ed
Maxime Michel <maxime.michel@magnolia-cms.com>
2021-07-14 15:36:50 +0200

    Ship some fields about the user for Magnolia SSO

--- server.js
+++ server.js
@@ -13,12 +13,30 @@ const config = ['CLIENT_ID', 'CLIENT_SECRET', 'CLIENT_REDIRECT_URI', 'CLIENT_LOG
 }, {})
 
 const oidcConfig = {
+  claims: {
+    email: ['email'],
+    profile: ['name', 'preferred_username']
+  },
   clients: [{
     client_id: config.clientId,
     client_secret: config.clientSecret,
     redirect_uris: [config.clientRedirectUri],
     post_logout_redirect_uris: [config.clientLogoutRedirectUri]
-  }]
+  }],
+  async findAccount (ctx, id) {
+    return {
+      accountId: id,
+      async claims (use, scope) {
+        return {
+          sub: id,
+          email: 'test@test.ch',
+          name: 'test',
+          preferred_username: 'Test'
+        }
+      }
+    }
+  }
+
 }
 
 const oidc = new Provider(`http://${host}:${port}`, oidcConfig)
